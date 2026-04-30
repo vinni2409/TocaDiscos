d8e123d3b5b1bc993e33d8bc7027047c7db8089a
vinni2409 <viniciusfurtado230@gmail.com>
2026-04-30 20:15:52 -0300

    Create Main and TocaDisco

--- src/TocaDisco.java
+++ src/TocaDisco.java
@@ -4,5 +4,63 @@ public class TocaDisco {
     private boolean tocando;
     private boolean pausado;
 
+    public TocaDisco() {
+        this.cdAtual = null;
+        this.ligado = false;
+        this.tocando = false;
+        this.pausado = false;
+    }
+
+    public void ligar() {
+        ligado = true;
+        System.out.println("Toca-discos ligado.");
+    }
+
+    public void desligar() {
+        stop();
+        ligado = false;
+        System.out.println("Toca-discos desligado.");
+    }
+
+    public void play(){
+        if(!ligado){
+            System.out.println("Ligue o toca discos primeiro");
+            return;
+        }
+        if(cdAtual == null){
+            System.out.println("insira o cd primeiro");
+            return;
+        }
+        tocando = true;
+        pausado = false;
+        System.out.println("Tocando: " + cdAtual.getTitulo() + " - " + cdAtual.getArtista());
+    }
+
+    public void pause() {
+        if (!tocando) {
+            System.out.println("Nenhuma música tocando.");
+            return;
+        }
+        tocando = false;
+        pausado = true;
+        System.out.println("Música pausada.");
+    }
+
+    public void stop() {
+        tocando = false;
+        pausado = false;
+        System.out.println("Reprodução parada.");
+    }
+
+    public void inserirCD(CD cd) {
+        this.cdAtual = cd;
+        System.out.println("CD inserido: " + cd.getTitulo());
+    }
+
+    public void ejetarCD() {
+        stop();
+        cdAtual = null;
+        System.out.println("CD ejetado.");
+    }
 
 }
--- src/main.java
+++ src/main.java
@@ -1,3 +1,17 @@
-public void main() {
+public class main {
+    public static void main(String[] args) {
 
+        CD cd1 = new CD("Thriller", "Michael Jackson", 9, 42);
+
+        TocaDisco aparelho = new TocaDisco();
+
+        aparelho.ligar();
+        aparelho.inserirCD(cd1);
+        aparelho.play();
+        aparelho.pause();
+        aparelho.play();
+        aparelho.stop();
+        aparelho.ejetarCD();
+        aparelho.desligar();
+    }
 }
